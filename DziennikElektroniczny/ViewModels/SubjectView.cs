c7cc309ddabb6507206e1b6f88bac44c368d525d
Piotr Szuba <sakonir@gmail.com>
2022-01-09 22:09:24 +0100

    subjestView change

--- DziennikElektroniczny/ViewModels/SubjectView.cs
+++ DziennikElektroniczny/ViewModels/SubjectView.cs
@@ -7,16 +7,25 @@ namespace DziennikElektroniczny.ViewModels
     {
         public int Id { get; set; }
         public int? SubjectInfoId { get; set; }
+        public string SubjectName { get; set; }
         public int? TeacherPersonId { get; set; }
+        public string TeacherName { get; set; } 
         public int StudentsGroupId { get; set; }
+        public string StudentsGroupName { get; set; }
         public int ClassRoomId { get; set; }
-        public SubjectView(Subject subject)
+        public string ClassRoomName { get; set; }
+        public SubjectView(Subject subject,SubjectInfo subjectInfo,ClassRoom classRoom,StudentsGroup studentsGroup,PersonalInfo teacherInfo)
         {
             Id = subject.SubjectId;
             SubjectInfoId = subject.SubjectInfoId;
+            SubjectName = subjectInfo.Title;
             TeacherPersonId = subject.TeacherPersonId;
+            TeacherName = teacherInfo.Name + " " + teacherInfo.Surname;
             StudentsGroupId = subject.StudentsGroupId;
+            StudentsGroupName = studentsGroup.Title;
             ClassRoomId = subject.ClassRoomId;
+            ClassRoomName = classRoom.Destination;
+
         }
     }
 }
