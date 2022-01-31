22cbcb3da8e2c62c269959c86cd8c8c0c07895ab
Szymon Kubiak <szymon.kubiak@altimi.com>
2022-01-31 23:27:10 +0100

    statistics service

---
+++ DziennikElektroniczny/AngularApp/frontend/src/app/services/statistics/statistics.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { GRADE_VALUES_NUMERIC_AND_WAGE } from 'src/app/constants';
+import { AttendanceViewModel } from 'src/app/models/Attendance';
+import { GradeViewModel } from 'src/app/models/Grade';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StatisticsService {
+  constructor() {}
+
+  public calculateGradeAverage(grades: GradeViewModel[]) {
+    let sum: number = 0;
+    let wageSum: number = 0;
+    grades.forEach((myGrade) => {
+      const grade = this.getGrade(myGrade.value);
+      sum += grade.numeric;
+      wageSum += grade.wage;
+    });
+    return sum / wageSum;
+  }
+
+  private getGrade(value: string) {
+    let grade = GRADE_VALUES_NUMERIC_AND_WAGE.find(
+      (grade) => grade.value == value
+    );
+    return grade!;
+  }
+
+  public calculateFrequency(attendances: AttendanceViewModel[]) {
+    const allCount = attendances.length;
+    let presentCount = 0;
+    attendances.forEach((at) => {
+      if (at.wasPresent) presentCount++;
+    });
+    return presentCount / allCount;
+  }
+}
